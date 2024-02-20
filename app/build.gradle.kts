28bb60aeccd2dff939dd36a0f24528b1acb2b920
Léo de Souza <dev@leodesouza.net>
2024-02-20 15:59:58 +0100

    Enable resource shrinking and app optimization

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,6 +18,8 @@ android {
     buildTypes {
         release {
             isMinifyEnabled = true
+            isShrinkResources = true
+            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"))
         }
     }
     compileOptions {
